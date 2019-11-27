d9b63663621b9b36f283821ec2e2aa614002a841
sitaramaprasad-e <55175202+sitaramaprasad-e@users.noreply.github.com>
2019-11-27 12:37:23 +0530

    Update deploymq.sh

--- MQ/deploymq.sh
+++ MQ/deploymq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-runmqsc PBCTEST <<STREAM
+runmqsc QMNAME <<STREAM
 def qlocal(EAMSR.REQUEST.FROM.INSERVICE) DESCR('Drop the eamsr message from Inservices to Maximo')  MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 def qlocal(EAMSR.REQUEST.FROM.INSERVICE.BACKOUT) DESCR('Drop the eamsr message from Inservices to Maximo') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 def qlocal(EAMSR.SERVICEORDER.LOGGING) DESCR('Logging queue') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
