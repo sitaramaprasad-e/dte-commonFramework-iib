258d918b18f8b2e3e25742cd79a2bd3b97d27de1
sitaramaprasad-e <55175202+sitaramaprasad-e@users.noreply.github.com>
2019-12-05 18:40:09 +0530

    Update deploymq.sh

--- MQ/deploymq.sh
+++ MQ/deploymq.sh
@@ -1,5 +1,5 @@
 QMGR=$1
-runmqsc $QMGR
+runmqsc $QMGR <<STREAM
 def qlocal(EAMSR.REQUEST.FROM.INSERVICE) DESCR('Drop the eamsr message from Inservices to Maximo')  MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 def qlocal(EAMSR.REQUEST.FROM.INSERVICE.BACKOUT) DESCR('Drop the eamsr message from Inservices to Maximo') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 def qlocal(EAMSR.SERVICEORDER.LOGGING) DESCR('Logging queue') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
@@ -7,3 +7,4 @@ def qlocal(OPI_INS_ADD_COMMENTS) DESCR('Inservice ServiceOrder updates queue') M
 def qlocal(EAMSR.REQUEST.RETRY) DESCR('Retry queue') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 def qlocal(EAMSR.REQUEST.LOGGING) DESCR('Logging queue') MAXDEPTH(10000) DEFPSIST(YES) REPLACE
 end
+STREAM
